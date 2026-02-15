085eb1972202f96926c954bab34c3c15c29e4828
Paulos Girmacew <paulosgirmachew28@gmail.com>
2026-02-15 23:01:05 +0300

    added a task adding counter for every task and modified the image

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-
-
 const displayCount=document.getElementById("count");
 const addButton=document.getElementById("btn");
 const displayText=document.getElementById("displayText");
 const span=document.getElementById("taskNo");
+const inputField=document.getElementById("inputs");
+const taskDisplay=document.getElementById("task");
 let count = Number(span.textContent);
+let addedTask="";
+
 
 
 function handleClick(){
- count ++;
- displayText.textContent=`Your current tasks ${count}`;
+     count ++;
 
-console.log(count);
-}
+     displayText.textContent=`Your current tasks 0/${count}`;
+    } 
+
 addButton.addEventListener("click",handleClick);
+
+addButton.addEventListener("click", function () {
+    addedTask=inputField.value;
+    taskDisplay.textContent=addedTask;
+    inputField.value="";
+
+});
+
+
