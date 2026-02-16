a46c622f87a40729ae7a6063105c2aadff2703d4
Paulos Girmacew <paulosgirmachew28@gmail.com>
2026-02-16 23:08:31 +0300

    Added empty task functionality and adding task using enterr key functionality

--- index.js
+++ index.js
@@ -4,24 +4,57 @@ const displayText=document.getElementById("displayText");
 const span=document.getElementById("taskNo");
 const inputField=document.getElementById("inputs");
 const taskDisplay=document.getElementById("task");
+
+const taskBox=document.querySelector("#task");
+const taskImage=document.querySelector(".emptyImage");
+const taskList= document.getElementById("task-list");
 let count = Number(span.textContent);
-let addedTask="";
+
 
 
 
 function handleClick(){
+     if(!inputField.value){
+        count=0;
+        alert("Please add a task");
+        taskBox.classList.add("hidden");
+        return;
+     }
      count ++;
+    const li=document.createElement("li");
+     li.textContent=inputField.value;
+     taskList.appendChild(li);
+     inputField.value="";
 
      displayText.textContent=`Your current tasks 0/${count}`;
     } 
 
-addButton.addEventListener("click",handleClick);
 
-addButton.addEventListener("click", function () {
-    addedTask=inputField.value;
-    taskDisplay.textContent=addedTask;
+
+/*addButton.addEventListener("click", function () {
+    if(!inputField.value){
+        
+        alert("Please add a task");
+        taskBox.classList.add("hidden");
+        return ;
+    }
+    
+    const li=document.createElement("li");
+    li.textContent=inputField.value;
+    taskList.appendChild(li);
     inputField.value="";
 
+   // addedTask=inputField.value;
+    //taskDisplay.textContent=addedTask;
+    //inputField.value="";  
+
+}); */
+
+addButton.addEventListener("click",handleClick);
+inputField.addEventListener("keypress",(e)=>{
+    if(e.key==="Enter"){
+        handleClick(e);
+    }
 });
 
 
